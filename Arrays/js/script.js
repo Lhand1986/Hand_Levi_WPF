5c7c699f3b82ed7be9e33603f8b3aa9f89632f21
Lhand1986 <lhand1986@yahoo.com>
2015-02-08 18:21:56 -0800

    Added variable, changed console
    Added Variable “num” and changed console output to reflect new variable

--- Arrays/js/script.js
+++ Arrays/js/script.js
@@ -5,5 +5,6 @@ var muppetNames = ["Kermit", "Miss Piggy"]; //0,1,2,3
 muppetNames[1] = "Rizzo"; // change index#1 to Rizzo
 muppetNames[2] = "Fozzy";
 
+var num = 2;
 
-console.log(muppetNames[2]); //lets me see what's in the index #1
+console.log(muppetNames[num]); //lets me see what's in the index #1
