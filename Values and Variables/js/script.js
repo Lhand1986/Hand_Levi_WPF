5a476a1e8c64d044986b9aaec2481408000a6c8e
Lhand1986 <lhand1986@yahoo.com>
2015-02-08 18:00:42 -0800

    Initialized and added statements
    Added statements to document and initialized variable yearOfBirth

--- Values and Variables/js/script.js
+++ Values and Variables/js/script.js
@@ -1,4 +1,7 @@
 // JavaScript, Defining Variables
 
-var age;
-age = 28;
+var age; //declaration
+age = 28; //definition
+//storage container = value
+
+var yearOfBirth = 1986; //declaring and defining on the same line
