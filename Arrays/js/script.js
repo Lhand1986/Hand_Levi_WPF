00977f76379a6be0952e9fbd1618f1bfdf97fbae
Lhand1986 <lhand1986@yahoo.com>
2015-02-08 18:21:06 -0800

    Added variable to array and changed output
    Added comments to section, added string “Fozzy” to variable array index
    number “2” and changed console output to array index number “2”

--- Arrays/js/script.js
+++ Arrays/js/script.js
@@ -2,7 +2,8 @@
 
 var muppetNames = ["Kermit", "Miss Piggy"]; //0,1,2,3
 
-muppetNames[1] = "Rizzo";
-console.log(muppetNames[1]);
+muppetNames[1] = "Rizzo"; // change index#1 to Rizzo
+muppetNames[2] = "Fozzy";
 
 
+console.log(muppetNames[2]); //lets me see what's in the index #1
