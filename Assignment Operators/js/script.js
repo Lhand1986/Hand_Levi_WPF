f86af5be44a842dbe47dc1d61202f79cb62be08b
Lhand1986 <lhand1986@yahoo.com>
2015-02-09 18:46:04 -0800

    Changed assignment operator
    Changed assignment operator to decrement operator “—“

--- Assignment Operators/js/script.js
+++ Assignment Operators/js/script.js
@@ -1,5 +1,6 @@
 //Expressions - Assignment Operators
 
 var a = 3;
-a ++; //a = a + 1 // a += 1 increment operator
+ //a = a + 1 // a += 1 increment operator
+a --: //a = a - 1 // a -+1 decrement operator
 console.log(a);
