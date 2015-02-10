a7bf81893effe16e111c4d6984da5822832ce673
Lhand1986 <lhand1986@yahoo.com>
2015-02-09 19:52:31 -0800

    Included additional variable with string
    Included additional variable “result” to include output of variable
    “area” with output to an alert

--- Prompts and alerts/js/script.js
+++ Prompts and alerts/js/script.js
@@ -3,4 +3,6 @@
 var width = prompt("We are calculating the area of a rectangle. \nPlease enter the width");
 var height = prompt("Please enter the height.");
 var area = width * height; //calculates with that info
-console.log(area); //prints it out to console
+//console.log(area); //prints it out to console
+var result = "The area of your rectangle is " + area + " sq feet"
+alert(result);
