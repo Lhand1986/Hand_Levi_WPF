f4c6a4c89f01530b40e66ef9e648b19efd1800d0
Lhand1986 <lhand1986@yahoo.com>
2015-02-09 19:49:41 -0800

    Added additional variable
    Added variable to include an expression for initial variables, print
    out to console with comments

--- Prompts and alerts/js/script.js
+++ Prompts and alerts/js/script.js
@@ -2,3 +2,5 @@
 
 var width = prompt("We are calculating the area of a rectangle. \nPlease enter the width");
 var height = prompt("Please enter the height.");
+var area = width * height; //calculates with that info
+console.log(area); //prints it out to console
