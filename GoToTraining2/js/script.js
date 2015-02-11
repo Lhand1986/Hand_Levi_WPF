86369c682f00afb96b5ede6e61a15dc44c842442
Lhand1986 <lhand1986@yahoo.com>
2015-02-10 18:37:24 -0800

    GoToTraining2
    Input data from Go To Training session 2, covering variables, prompts,
    and output.

---
+++ GoToTraining2/js/script.js
@@ -0,0 +1,23 @@
+/*
+ Levi Hand
+ SDI 1502
+ Section 2
+ First foray into code
+*/
+
+// Add alert to test the file
+//alert("JavaScript works!");
+
+// Variables and Data Types
+//var myAddress = "1453 Colorado Blvd";
+//var milesToStore = 15;
+//var isThereConstruction = true;
+var myFirstName;
+var myLastName;
+
+// Prompts
+myFirstName = prompt("Please enter your first name");
+myLastName = prompt("Please enter your last name");
+
+// Outputs
+console.log("Your name is: " + myFirstName + " " + myLastName +  ".");
