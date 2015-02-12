21d0afc9fa11eae07552af6179eb8eb8affb6e04
Lhand1986 <lhand1986@yahoo.com>
2015-02-11 18:00:31 -0800

    Expressions assignment week 2 start
    Created the framework to start the calculator assignment with notes
    added describing what the desired outcome is.

---
+++ Hand_Levi_Expressions_Assignment/js/script.js
@@ -0,0 +1,12 @@
+/*
+ Levi Hand
+ SDI 1502
+ Section 2
+ Expressions Assignment
+ */
+
+/*
+Creating a calculator which will calculate the number of bars necessary for a merge tag
+to be properly printed at a steel mill. Also creating a calculating system for how many
+drops make a full run to cut bundles into the proper size.
+ */
