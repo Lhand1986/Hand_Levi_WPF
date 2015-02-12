ab07c16288d1daf0977e1f54441df7cca080cd92
Lhand1986 <lhand1986@yahoo.com>
2015-02-11 18:24:42 -0800

    Added expression
    Added expression to formulate how many bars from the first heat of bars
    will remain on the last run.

--- Hand_Levi_Expressions_Assignment/js/script.js
+++ Hand_Levi_Expressions_Assignment/js/script.js
@@ -11,17 +11,22 @@ to be properly printed at a steel mill. Also creating a calculating system for h
 drops make a full run to cut bundles into the proper size.
  */
 
- mergeNumber = [4];
+mergeNumber = [4];
 var numberOfBars = "Enter the total number of bars per bundle.";
 var barsInRun;
-
 var mergeNotification = "The number of bars you need to merge from the next heat is ";
 
+// Initializing prompts to input data into the array variables for the merge number.
 mergeNumber[0] = prompt("Enter the number of drops remaining on the hot bed");
 mergeNumber[1] = prompt("Enter the total number of bars per drop");
 mergeNumber[2] = prompt("Enter the number of cuts to make a full bundle");
 mergeNumber[3] = prompt("Enter the total number of bars that make a full bundle");
-console.log(mergeNumber[0]);
-console.log(mergeNumber[1]);
-console.log(mergeNumber[2]);
-console.log(mergeNumber[3]);
+//console.log(mergeNumber[0]);
+//console.log(mergeNumber[1]);
+//console.log(mergeNumber[2]);
+//console.log(mergeNumber[3]);
+
+// Multiplying variables in order to get the number of bars remaining in the run.
+mergeNumber[4] = mergeNumber[0] *  mergeNumber[1] * mergeNumber[2];
+console.log(mergeNumber[4]);
+
