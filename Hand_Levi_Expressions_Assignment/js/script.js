9e95218893ea199973200924ea64b78776bfe436
Lhand1986 <lhand1986@yahoo.com>
2015-02-11 18:09:13 -0800

    Initialized variables and first prompt
    Initialized variables (array) mergeNumber, (string) numberOfBars,
    barsInRun, and (string) mergeNotification. Added a prompt to provide
    user input to mergeNumber[0]

--- Hand_Levi_Expressions_Assignment/js/script.js
+++ Hand_Levi_Expressions_Assignment/js/script.js
@@ -10,3 +10,13 @@ Creating a calculator which will calculate the number of bars necessary for a me
 to be properly printed at a steel mill. Also creating a calculating system for how many
 drops make a full run to cut bundles into the proper size.
  */
+
+ mergeNumber = [4];
+var numberOfBars = "Enter the total number of bars per bundle.";
+var barsInRun;
+
+var mergeNotification = "The number of bars you need to merge from the next heat is ";
+
+mergeNumber[0] = prompt("Enter the number of drops remaining on the hot bed")
+
+console.log(mergeNumber[0]);
