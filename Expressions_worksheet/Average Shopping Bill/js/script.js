32a1a8bdc381e6a3a78bdf3e3a4667451c50a4ff
Lhand1986 <lhand1986@yahoo.com>
2015-02-12 18:11:32 -0800

    Initializing variables and array
    Initializing variables and array

---
+++ Expressions_worksheet/Average Shopping Bill/js/script.js
@@ -0,0 +1,13 @@
+/*
+ Levi Hand
+ SDI 1502
+ Section 2
+ Average Shopping Bill Problem
+ */
+
+//Initializing array variable with 5 places
+var groceryTotals = [5];
+//Initializing variables without assigning values
+var groceryAverage;
+var groceryFinal;
+
