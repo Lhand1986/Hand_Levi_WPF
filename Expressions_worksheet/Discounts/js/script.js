5a18f24b5a0bace983eb4fc2e01d3dbdca4ffa1e
Lhand1986 <lhand1986@yahoo.com>
2015-02-12 18:41:59 -0800

    Discounts problem initialized
    Initialized variables and provided prompts for user input

---
+++ Expressions_worksheet/Discounts/js/script.js
@@ -0,0 +1,15 @@
+/*
+ Levi Hand
+ SDI 1502
+ Section 2
+ Discounts problem
+ */
+
+//Initializing variables without assigning values
+var discountedTotalWithoutTax;
+var discountedTotalTaxed;
+//Initializing variables with prompt for user input
+var originalPrice = prompt("Enter the original price of the item");
+var discountPercentage = prompt("Enter the percentage being discounted");
+var itemDescription = prompt("Enter a brief description of the item");
+var salesTax = prompt("Enter the sales tax percentage");
