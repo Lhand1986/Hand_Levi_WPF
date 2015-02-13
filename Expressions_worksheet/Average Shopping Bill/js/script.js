d9eb8469e2987f63f85e86e3b2abaf0776d079b9
Lhand1986 <lhand1986@yahoo.com>
2015-02-12 18:15:25 -0800

    Added prompts
    Added prompts for user input, tested with console.log and commented out
    the console.

--- Expressions_worksheet/Average Shopping Bill/js/script.js
+++ Expressions_worksheet/Average Shopping Bill/js/script.js
@@ -11,3 +11,16 @@ var groceryTotals = [5];
 var groceryAverage;
 var groceryFinal;
 
+//Prompts for user input to assign value to array
+groceryTotals[0] = prompt("Enter the totals for the first week of grocery shopping");
+groceryTotals[1] = prompt("Enter the totals for the second week of grocery shopping");
+groceryTotals[2] = prompt("Enter the totals for the third week of grocery shopping");
+groceryTotals[3] = prompt("Enter the totals for the fourth week of grocery shopping");
+groceryTotals[4] = prompt("Enter the totals for the fifth week of grocery shopping");
+//Debugging purposes
+//console.log(groceryTotals[0]);
+//console.log(groceryTotals[1]);
+//console.log(groceryTotals[2]);
+//console.log(groceryTotals[3]);
+//console.log(groceryTotals[4]);
+
