1f296e53a521a1e999fae80729f7d5751d205c65
Lhand1986 <lhand1986@yahoo.com>
2015-02-12 17:59:11 -0800

    Slice of pie problem 2
    Starting with script from slice of pie problem, added modulus operator

---
+++ Expressions_worksheet/Slice of pie 2/js/script.js
@@ -0,0 +1,26 @@
+/*
+ Levi Hand
+ SDI 1502
+ Section 2
+ Slice of Pie Problem 2
+ */
+
+//Initializing variables without assigning values
+var numberOfSlices;
+var numberOfPeople;
+var numberOfPizzas;
+var slicesEaten;
+
+//Assigning user input to variables via prompt
+numberOfSlices = prompt("Enter the number of slices per pizza");
+numberOfPeople = prompt("Enter the number of people at the party");
+numberOfPizzas = prompt("Enter the number of pizzas ordered");
+//Debugging output
+//console.log(numberOfSlices);
+//console.log(numberOfPeople);
+//console.log(numberOfPizzas);
+
+//Expression to assign equation output to variable
+slicesEaten = numberOfPizzas * numberOfSlices % numberOfPeople;
+
+console.log(slicesEaten);
