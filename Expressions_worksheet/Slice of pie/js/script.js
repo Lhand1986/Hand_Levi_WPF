5fb319d7d0fa31c021c45bfe8ab2e2f8ef82a4bf
Lhand1986 <lhand1986@yahoo.com>
2015-02-12 17:51:54 -0800

    Expression and alert added
    Assigned an expression to provide variable output as well as assigning
    a concatenation string to an alert

--- Expressions_worksheet/Slice of pie/js/script.js
+++ Expressions_worksheet/Slice of pie/js/script.js
@@ -16,6 +16,12 @@ numberOfSlices = prompt("Enter the number of slices per pizza");
 numberOfPeople = prompt("Enter the number of people at the party");
 numberOfPizzas = prompt("Enter the number of pizzas ordered");
 //Debugging output
-console.log(numberOfSlices);
-console.log(numberOfPeople);
-console.log(numberOfPizzas);
+//console.log(numberOfSlices);
+//console.log(numberOfPeople);
+//console.log(numberOfPizzas);
+
+//Expression to assign equation output to variable
+slicesEaten = numberOfPizzas * numberOfSlices / numberOfPeople;
+
+//Alert output of concatenation of strings with a number variable.
+alert("Each person ate " + slicesEaten + " slices of pizza at the party.");
