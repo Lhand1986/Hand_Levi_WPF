c7035e019a7f613786f27ffb3a4fd02e65504a5e
Lhand1986 <lhand1986@yahoo.com>
2015-02-12 18:51:27 -0800

    Added expression to assign total with discount
    Added expression to assign total with discount

--- Expressions_worksheet/Discounts/js/script.js
+++ Expressions_worksheet/Discounts/js/script.js
@@ -13,3 +13,12 @@ var originalPrice = prompt("Enter the original price of the item");
 var discountPercentage = prompt("Enter the percentage being discounted");
 var itemDescription = prompt("Enter a brief description of the item");
 var salesTax = prompt("Enter the sales tax percentage");
+
+//Expression to calculate the discount
+discountedTotalWithoutTax = discountPercentage / 100 * originalPrice;
+
+//Expression to assign the discounted total to the variable
+discountedTotalWithoutTax = originalPrice - discountedTotalWithoutTax;
+
+//Debugging purposes
+//console.log(discountedTotalWithoutTax);
