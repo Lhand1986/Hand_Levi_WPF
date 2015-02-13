dc53deb1075f6ac0270728b3a1564cbca98dc7c7
Lhand1986 <lhand1986@yahoo.com>
2015-02-12 18:31:32 -0800

    Added the final expressions
    Added the final expression for division to provide an average, as well
    as an alert with a concatenation of string and variable to provide
    output.

--- Expressions_worksheet/Average Shopping Bill/js/script.js
+++ Expressions_worksheet/Average Shopping Bill/js/script.js
@@ -24,8 +24,14 @@ groceryTotals[4] = prompt("Enter the totals for the fifth week of grocery shoppi
 //console.log(groceryTotals[3]);
 //console.log(groceryTotals[4]);
 
-//Expression to add all the totals of the array together
+//Expression to add all the totals of the array together.
+//Had a problem initially with the prompt entering the numbers as string variables.
+//Utilized casting in order to identify to the program that the variables are indeed numbers.
 groceryFinal = Number(groceryTotals[0]) + Number(groceryTotals[1]) + Number(groceryTotals[2]) + Number(groceryTotals[3]) + Number(groceryTotals[4]);
 
 //Debugging purposes
-console.log(groceryFinal);
+//console.log(groceryFinal);
+
+groceryAverage = groceryFinal / 5;
+
+alert("You have spent a total of $" + groceryFinal + " on groceries over 5 weeks. That is an average of $" + groceryAverage + " per week");
