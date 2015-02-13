b3e7a9e17b38ca1814c9061dc8d7ee703b8fb7b5
Lhand1986 <lhand1986@yahoo.com>
2015-02-12 17:46:29 -0800

    Slice of pie start
    Initialized variables without assigning value

---
+++ Expressions_worksheet/Slice of pie/js/script.js
@@ -0,0 +1,13 @@
+/*
+ Levi Hand
+ SDI 1502
+ Section 2
+Slice of Pie Problem
+ */
+
+//Initializing variables without assigning values
+var numberOfSlices;
+var numberOfPeople;
+var numberOfPizzas;
+var slicesEaten;
+
