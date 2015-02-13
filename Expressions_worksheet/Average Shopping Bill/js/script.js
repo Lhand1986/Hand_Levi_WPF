49c43870981a019e1c1a8cfd56fc7e8d26834c31
Lhand1986 <lhand1986@yahoo.com>
2015-02-12 18:21:53 -0800

    Commenting out for debugging
    Problem with the expression adding the numbers in the array together as
    a concatenation. Attempting to assign numbers to the array and add them
    directly to see what happens.

--- Expressions_worksheet/Average Shopping Bill/js/script.js
+++ Expressions_worksheet/Average Shopping Bill/js/script.js
@@ -6,17 +6,17 @@
  */
 
 //Initializing array variable with 5 places
-var groceryTotals = [5];
+var groceryTotals = [1, 2, 3, 4, 5];
 //Initializing variables without assigning values
 var groceryAverage;
 var groceryFinal;
 
 //Prompts for user input to assign value to array
-groceryTotals[0] = prompt("Enter the totals for the first week of grocery shopping");
-groceryTotals[1] = prompt("Enter the totals for the second week of grocery shopping");
-groceryTotals[2] = prompt("Enter the totals for the third week of grocery shopping");
-groceryTotals[3] = prompt("Enter the totals for the fourth week of grocery shopping");
-groceryTotals[4] = prompt("Enter the totals for the fifth week of grocery shopping");
+//groceryTotals[0] = prompt("Enter the totals for the first week of grocery shopping");
+//groceryTotals[1] = prompt("Enter the totals for the second week of grocery shopping");
+//groceryTotals[2] = prompt("Enter the totals for the third week of grocery shopping");
+//groceryTotals[3] = prompt("Enter the totals for the fourth week of grocery shopping");
+//groceryTotals[4] = prompt("Enter the totals for the fifth week of grocery shopping");
 //Debugging purposes
 //console.log(groceryTotals[0]);
 //console.log(groceryTotals[1]);
@@ -24,3 +24,8 @@ groceryTotals[4] = prompt("Enter the totals for the fifth week of grocery shoppi
 //console.log(groceryTotals[3]);
 //console.log(groceryTotals[4]);
 
+//Expression to add all the totals of the array together
+groceryFinal = groceryTotals[0] + groceryTotals[1] + groceryTotals[2] + groceryTotals[3] + groceryTotals[4];
+
+//Debugging purposes
+console.log(groceryFinal);
