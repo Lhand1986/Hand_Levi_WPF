d7b3c661c8d8e7a30501cee4ba08e5d31bfff631
Lhand1986 <lhand1986@yahoo.com>
2015-02-12 17:39:14 -0800

    Starting dog years expression
    Initialized variables with a user input prompt and output user data to
    console.log. Commented out console.log output

---
+++ Expressions_worksheet/Dog Years/js/script.js
@@ -0,0 +1,15 @@
+/*
+ Levi Hand
+ SDI 1502
+ Section 2
+ Dog Years Expression Problem
+ */
+
+//Initializing variable with the number 7
+var dogConversion = 7;
+//Initializing variable without assigning a value
+var dogYears;
+//Initializing a variable with a user input prompt
+var sparkyAge = prompt("Enter Sparky's current age in human years.");
+//Output variable to console for debugging purposes
+//console.log(sparkyAge);
