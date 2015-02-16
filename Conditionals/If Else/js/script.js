ed8294a1536f5e17c60e0e0bf8173dc9a7f80b4a
Lhand1986 <lhand1986@yahoo.com>
2015-02-15 20:23:36 -0800

    If Else statements
    If Else statement with example

--- Conditionals/If Else/js/script.js
+++ Conditionals/If Else/js/script.js
@@ -1,12 +1,14 @@
-//Conditional Logic - With an Expression
+//Conditional Logic - With an Expression - If Else
 
-var kidHeight = 47;
+var kidHeight = 52;
 var minHeight = 48;
 
 
 //If the child is old enough, print to the console "you can ride!"
 //if the kid is over 48 inches in height
-if(kidHeight + sneakerLift > minHeight){
+if(kidHeight > minHeight){
     //code performed if condition is true
     console.log("You can ride the coaster!");
+}else{
+    console.log("Sorry kid, you've got some growing to do first!");
 }
