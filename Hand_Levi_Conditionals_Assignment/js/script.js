6f2fafc013c94b2110d0ce5a88a530b5bfd969da
Lhand1986 <lhand1986@yahoo.com>
2015-02-17 19:15:39 -0800

    Conditionals assignment start
    Initialized variables with givens and static numerical variables.
    Notations made.

---
+++ Hand_Levi_Conditionals_Assignment/js/script.js
@@ -0,0 +1,23 @@
+/*
+    Levi Hand
+    SDI 1502
+    Section 2
+    Week 3
+    Conditionals Assignment
+ */
+
+/*
+    Create a calculator which calculates how many stops you would have to make
+    during a long trip. The outcome is dictated by what variety of vehicle you
+    are using, whether it is a car with an average distance of 300 miles per
+    tank of gas, or a motorcycle with an average distance of 150 miles per
+    tank of gas. Also, the calculator will take into account what percentage
+    of a full tank you currently possess.
+ */
+
+var vehicleType;                //Filled using a prompt to determine what kind of vehicle is being used
+var vehicleMotorcycle = 150;    //Static variable which denotes 150 miles for the motorcycle
+var vehicleCar =        300;    //Static variable which denotes 300 miles for the car
+var distanceTraveling;          //Filled using a prompt to determine what the distance of the trip is
+var percentCurrentGas;          //Variable filled using a prompt to denote what percentage of gas the vehicle currently has
+
