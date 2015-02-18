b8da62a51c162daafe71ad2302b8fafc1a151830
Lhand1986 <lhand1986@yahoo.com>
2015-02-18 00:10:12 -0800

    Added statements for calculation
    Moved statements to better fit the layout of the program, added
    mathematical equations to figure out distances and stops, and a final
    output to the console.log

--- Hand_Levi_Conditionals_Assignment/js/script.js
+++ Hand_Levi_Conditionals_Assignment/js/script.js
@@ -21,6 +21,8 @@ var vehicleCar =        300;    //Static variable which denotes 300 miles for th
 var distanceTraveling;          //Filled using a prompt to determine what the distance of the trip is
 var percentCurrentGas;          //Variable filled using a prompt to denote what percentage of gas the vehicle currently has
 var currentGasDistance;
+var travelDistance;
+var travelStops;
 
 //Prompting the user for input on what variety of vehicle they are using
 vehicleType = prompt("Are you going to be driving a Motorcycle or a Car?");
@@ -41,21 +43,35 @@ if(vehicleType != "Motorcycle" && vehicleType != "Car"){
         //Output if the statement is true
         console.log("You need to enter a positive number for your travel distance!");
     } else {
+        //If the statement is false
+
         //Prompting the user for input on how full their gas tank is as a percentage
         percentCurrentGas = prompt("Enter the how full your tank currently is as a percent.");
+
         //Convert variable percentCurrentGas to a float variable for easier calculation
         percentCurrentGas = parseFloat(percentCurrentGas);
-        //Output if the statement is false
+
         //Output to tell the user how many miles they will be traveling
         console.log("You are traveling a total distance of " + distanceTraveling + " miles.");
+
         if(isNaN(percentCurrentGas) || percentCurrentGas > 100 ){
             //Output if the statement is true
+
             console.log("You need to enter a positive number below 101 for remaining fuel!")
         } else {
             //Output to tell the user how much gas they currently have as a percentage
             console.log("You currently have " + percentCurrentGas + "% of a tank remaining");
+
+            //Figure out how much distance you can travel with the amount of fuel currently available
+            currentGasDistance = (vehicleType === "Motorcycle") ? percentCurrentGas / 100 * vehicleMotorcycle : percentCurrentGas / 100 * vehicleCar;
+
+            //Statement to determine the total distance to travel
+            travelDistance = currentGasDistance + distanceTraveling;
+            //Ternary statement determines how many stops will have to be made along the way to refuel
+            travelStops = (vehicleType === "Motorcycle") ? travelDistance / vehicleMotorcycle : travelDistance / vehicleCar;
+            //Output how many stops need to be made to the console.log
+            console.log("You will need to make " + travelStops + " stops for fuel in order to complete your trip.");
+
         }
     }
 }
-//Figure out how much distance you can travel with the amount of fuel currently available
-currentGasDistance = (vehicleType === "Motorcycle") ? percentCurrentGas / 100 * vehicleMotorcycle : percentCurrentGas / 100 * vehicleCar;
