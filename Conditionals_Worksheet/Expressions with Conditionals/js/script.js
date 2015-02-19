adcdcccc211e53104fa6450128fd89f86cd72d50
Lhand1986 <lhand1986@yahoo.com>
2015-02-19 00:36:18 -0800

    Finished worksheet section
    Finished worksheet section with final conditional statement with
    output. Also added comments to other sections.

--- Conditionals_Worksheet/Expressions with Conditionals/js/script.js
+++ Conditionals_Worksheet/Expressions with Conditionals/js/script.js
@@ -39,18 +39,24 @@ if(isNaN(gasEfficiency) || gasEfficiency === ""){
     //Change variable to a float type integer
     gasReading = parseFloat(gasReading);
 
+    //Conditional statement checking if the variable is in number form
     if(isNaN(gasReading)){
+        //Output if the condition is true
         console.log("Please enter a number for your remaining fuel percentage.");
 
+        //Code continues if the statement is false
     } else {
         //Prompt user for input
         gasCapacity = prompt("What is the capacity of your gas tank?");
         //Change variable to a float type integer
         gasCapacity = parseFloat(gasCapacity);
 
+        //Conditional statement checking to see if variable is in number form
         if(isNaN (gasCapacity)){
+            //Output if the statement is true.
             console.log("Please enter the capacity of your gas tank.");
 
+            //Conditional statement to determine if the variable is less than 0
         } else if(gasCapacity <= 0){
             console.log("Please enter a positive number for the capacity of your gas tank.");
         } else {
@@ -60,6 +66,13 @@ if(isNaN(gasEfficiency) || gasEfficiency === ""){
             //Equation to determine what the total range of the vehicle is.
             totalMiles = totalGas * gasEfficiency;
             //console.log(totalMiles);
+
+            //conditional statement to create output for program
+            if(totalMiles >= 200){
+                console.log("Yes, you can make it without stopping for gas!");
+            } else {
+                console.log("You only have " + totalGas + " gallons of gas in your tank, better get gas now while you can!");
+            }
 
         }
     }
