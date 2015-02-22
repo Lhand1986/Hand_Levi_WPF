45842a8f624a9f1a94db9d9ed520f660f19b91c0
Lhand1986 <lhand1986@yahoo.com>
2015-02-22 13:52:03 -0800

    Anonymous Functions ex
    Anonymous Functions example

---
+++ Functions/Anonymous Functions/js/script.js
@@ -0,0 +1,10 @@
+//Functions - Anonymous Functions
+
+var calcArea = function(width, height){ //defining function
+    var area = width * height;
+    return area;
+}
+
+var a = calcArea(20, 30); //calling function
+
+console.log(a);
