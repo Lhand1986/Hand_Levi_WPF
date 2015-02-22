8464f9dfe9274c6aa3cd66653bd9ef9fb1b25b3a
Lhand1986 <lhand1986@yahoo.com>
2015-02-22 12:52:32 -0800

    Function Execution
    Function Execution example

---
+++ Functions/Function Execution/js/script.js
@@ -0,0 +1,19 @@
+//Functions - Function Execution
+
+
+function outptMsg(){
+    console.log("Hello World!");
+}
+
+//Function definition
+function calcArea(){
+    var width = 20;
+    var height = 30;
+    var area = width * height;
+    console.log(area);
+}
+
+//Function execution / call / invocation
+calcArea();
+calcArea();
+calcArea();
