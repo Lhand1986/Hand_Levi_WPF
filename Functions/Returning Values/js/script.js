39c6d0c48bc3411d5bafb59e17035ec445cbf3e9
Lhand1986 <lhand1986@yahoo.com>
2015-02-22 13:35:55 -0800

    Functions - Returning Values
    Functions - Returning Values example

---
+++ Functions/Returning Values/js/script.js
@@ -0,0 +1,15 @@
+//Functions - Returning Values
+
+/*
+ When the function returns a value,
+ it returns the value to the area
+ where the argument is presented.
+ */
+
+var total = calcArea(30, 20);
+
+function calcArea(w, h){
+    var area = w * h;
+    return area; //function spitting the info out
+}
+console.log(total);
