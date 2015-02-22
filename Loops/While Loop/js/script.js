b8b65e1cfe6966927cc872d46bef7bfd040eb932
Lhand1986 <lhand1986@yahoo.com>
2015-02-22 15:20:21 -0800

    Added do while example
    Added do while example

--- Loops/While Loop/js/script.js
+++ Loops/While Loop/js/script.js
@@ -1,8 +1,19 @@
 //While Loops
+console.log("------------Loops------------")
 
-var b = 50; //sets up the index
+var b = 10; //sets up the index
 
 while (b > 0){ //checks the condition
     console.log(b + " kegs on the wall.");
     b--; //increments or decrements the index
 }
+
+console.log("------------ Do While Loops ------------")
+
+var c = 10;
+
+do{ // Runs the code before checking the condition
+    console.log(c + " kegs on the wall");
+    c--;
+}
+while(c > 0); // As long as this condition is true
