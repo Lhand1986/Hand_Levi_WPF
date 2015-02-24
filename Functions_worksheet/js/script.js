4731227e3d37f2c52ab4fef6b4ee63e2c2004939
Lhand1986 <lhand1986@yahoo.com>
2015-02-23 19:50:49 -0800

    Started Functions Worksheet
    Started functions worksheet, input example

---
+++ Functions_worksheet/js/script.js
@@ -0,0 +1,16 @@
+/*
+ Levi Hand
+ SDI 1502
+ Section 2
+ Functions Worksheet
+ */
+
+//Example expression
+var width = 5;
+var height = 6;
+var area = calculateArea(width, height);
+console.log("The Area of the Rectangle is " + area);
+
+function calculateArea(w, h){
+    return w * h;
+}
