e36be835a152c58e06c008262e70b118dd1a233b
Lhand1986 <lhand1986@yahoo.com>
2015-02-24 10:43:41 -0800

    GTT Week 4 function start
    Starting function

--- Go To Training 4/js/script.js
+++ Go To Training 4/js/script.js
@@ -5,11 +5,27 @@
 //alert("JavaScript works!");
 
 //Week 3 review
-var myNumber = prompt("Please enter a number:");
+//var myNumber = prompt("Please enter a number:");
 
-if (myNumber === "" || isNaN(myNumber)){
-    myNumber = prompt("We need a number here.\nPlease enter a number!")
+//if (myNumber === "" || isNaN(myNumber)){
+//    myNumber = prompt("We need a number here.\nPlease enter a number!")
+//}
+
+//console.log("Your number is " + myNumber);
+
+//End week 3
+
+//Week 4
+
+// variables
+
+
+// functions
+function nameValidation() {
+
+    console.log("My function works!");
+
 }
 
-console.log("Your number is " + myNumber);
-
+// main code
+nameValidation();
