102b69ff34964f4df2fa7cf1c39009d5a9c69c7d
Levi Hand <lhand1986@yahoo.com>
2015-02-25 23:28:05 -0800

    Starting Stung!
    Starting Stung! worksheet project. Initialized variable animalWeight,
    and added initial comments

--- Functions_worksheet/js/script.js
+++ Functions_worksheet/js/script.js
@@ -28,3 +28,15 @@ console.log("The circumference of the circle is " + circumference);
 function calculateCirc(r, c){
     return r * c * Math.PI;
 }
+
+//STUNG!
+
+/*
+ It takes 8.666666667 bee stings per pound to kill an animal.
+ Calculate how many bee stings are needed to kill an animal
+ in a function.
+ */
+
+//Initialize variable for weight
+var animalWeight;
+
